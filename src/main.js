14d97a6044adaeb905e6e0f2e38d8d3a844f447a
Vongola <1085280247@qq.com>
2017-08-11 17:34:08 +0800

    引入element组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+
+// mardown编辑器
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+
 Vue.use(mavonEditor)
 Vue.config.productionTip = false
 
