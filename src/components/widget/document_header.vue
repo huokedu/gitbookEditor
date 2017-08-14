989b4d8bc25df8d6b449548827acc8e290917481
Vongola <1085280247@qq.com>
2017-08-14 10:33:25 +0800

    完成document-header

--- src/components/widget/document_header.vue
+++ src/components/widget/document_header.vue
@@ -1,18 +1,44 @@
 <template>
   <div id="header">
-    <h2>APIplus技术文档</h2>
+    <h2 contenteditable="true">APIplus技术文档</h2>
+    <span>
+      <i class="el-icon-document"></i><br>
+      保存
+    </span>
   </div>
 </template>
 
 <script>
+import {Icon} from 'element-ui'
 export default {
-  name: 'repo_document'
+  name: 'document_header',
+  components: {
+    Icon
+  }
 }
 </script>
 
-<style>
+<style scoped>
  #header {
-  width: 100%;
-  height: 80px;
+  padding: 1px;
+ }
+ #header h2 {
+  display: inline-block;
+  margin: 30px 20px;
+  font-size: 24px;
+ }
+ #header span {
+   float: right;
+   margin: 22px 22px 0;
+   line-height: 25px;
+   font-size: 14px;
+   text-align: center;
+   cursor: pointer;
+ }
+ #header span:hover {
+  color: #f63;
+ }
+ #header span .el-icon-document{
+   font-size: 20px;
  }
 </style>
