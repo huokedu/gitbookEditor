a94871d4359fadb8b8f31ba86b9347c3ef0fbd6f
Vongola <1085280247@qq.com>
2017-08-15 18:19:19 +0800

    完成添加分类

--- src/components/widget/list-collection.vue
+++ src/components/widget/list-collection.vue
@@ -1,25 +1,65 @@
 <template>
   <div id="listCol">
-    <h2><i class="el-icon-plus"></i>&nbsp;&nbsp;新分类</h2>
+    <h2 @click="addSort"><i class="el-icon-plus"></i>&nbsp;&nbsp;新分类</h2>
     <div class="collection">
-       <span class="col" v-for="col of collection" :key="col">
+       <span class="col" v-for="(col,index) of collection" :key="col" >
         <span :title="col">
           {{col}}        
         </span>
-        <i class="el-icon-delete2" title="删除分类"></i>        
-        <i class="el-icon-plus" title="添加文档"></i>
+        <i class="el-icon-delete2" title="删除分类"　@click="delSort(index)"></i>        
       </span>
     </div>
   </div>
 </template>
 
 <script>
-import {Icon} from 'element-ui'
+import {Icon, MessageBox, Message} from 'element-ui'
 export default {
   name: 'list-collection',
   data () {
     return {
       collection: ['fasdf', 'asdfasdf', '你是大厦发斯蒂芬']
+    }
+  },
+  methods: {
+    delSort (index) {
+      let vm = this
+      MessageBox.confirm('此操作将删除该分类下所有文章, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        vm.collection.splice(index, 1)
+        Message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    addSort () {
+      let vm = this
+      MessageBox.prompt('请输入分类名称', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /\D/,
+        inputErrorMessage: '分类名不能有数字'
+      }).then(({ value }) => {
+        vm.collection.splice(vm.collection.length, 1, value)
+        Message({
+          type: 'success',
+          message: '添加成功'
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '取消输入'
+        })
+      })
     }
   },
   components: {
@@ -52,7 +92,7 @@ export default {
 }
 #listCol .col span{
   display: inline-block;
-  width: 100px;
+  width: 120px;
   overflow:hidden;
   white-space:nowrap;
   text-overflow:ellipsis;
