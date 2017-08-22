987c66e2274c5f0cd6a2e2c47d3075f3de084888
Vongola <1085280247@qq.com>
2017-08-22 09:21:22 +0800

    添加页面大小限制

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@ export default {
 </script>
 
 <style>
+html {
+  min-width: 1330px;
+}
 #app {
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
