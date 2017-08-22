bb66881475d5ed48095a299f6bf74f5f905dd2cc
Vongola <1085280247@qq.com>
2017-08-22 09:25:38 +0800

    修改文章编辑接口设置

--- src/components/widget/editor.vue
+++ src/components/widget/editor.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="mainBook"> 
-    <mavon-editor style="height: 100%" v-model="value"  @save="save" @imgAdd="saveImg" @imgDel="imgDel" :toolbars = "toolbars" />
+    <mavon-editor style="height: 100%" v-model="value"/>
+    <!-- @save="save" @imgAdd="saveImg" @imgDel="imgDel" :toolbars = "toolbars" -->
   </div>
 </template>
 
 <script>
-import Axios from 'axios'
+import { getContent } from '../../js/axios.js'
 export default {
   name: 'hello',
+  props: ['id'],
   data () {
     return {
       value: '',
@@ -39,70 +41,67 @@ export default {
   },
   mounted () {
     const vm = this
-    Axios.defaults.withCredentials = true
-    Axios.get('http://192.168.1.98:4000/v1/article/query', {
-      params: {
-        id: '598542f7e3995d378ec0b829'
-      }
-    })
-    .then(res => {
-      if (res.data.status === 200) vm.value = res.data.data.article.content
-    })
-    Axios.post('http://192.168.1.98:4000/v1/account/login', {
-      username: '1111',
-      password: '1234'
-    }, {
-      withCredentials: true
-    })
-    .then(res => {
-      console.log(res)
-    })
+    vm.getContent()
   },
   methods: {
-    save (input, mk) {
-      const vm = this
-      Axios.post('http://192.168.1.98:4000/v1/article/edit', {
-        id: '598542f7e3995d378ec0b829',
-        content: vm.value,
-        pics: vm.img_file
+    // 获取文章详情
+    getContent () {
+      let vm = this
+      getContent(vm.id).then(res => {
+        if (res.data.status === 200) vm.value = res.data.data.article.content
+      }).catch((err) => {
+        console.log(err)
       })
-      .then(function (res) {
-        console.log(res)
-      })
-    },
-    saveImg (name, $file) {
-      const vm = this
-      let tem = name
-      const img = document.querySelector('img[rel="' + name + '"]')
-      const image = new Image()
-      image.onload = () => {
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-        const w = canvas.width = image.width
-        const h = canvas.height = image.height
-        ctx.drawImage(image, 0, 0, w, h, 0, 0, w, h)
-        const data = canvas.toDataURL('image/jpeg', 0.5)
-        // 去重
-        Object.keys(vm.img_file).map((pos) => {
-          if (vm.img_file[pos] === data) {
-            const reg = new RegExp('(!\\[[\\w\\W]+?\\]\\()' + name + '(\\))', 'g')
-            vm.value = vm.value.replace(reg, '$1' + pos + '$2')
-            tem = pos
-            return
-          }
-        })
-        if (tem === name) {
-          console.log(1)
-          vm.img_file[name] = data
-        }
-      }
-      image.src = img.src
-    },
-    imgDel (pos) {
-      const vm = this
-      delete this.img_file[pos]
-      const reg = new RegExp('!\\[[\\w\\W]+?\\]\\(' + pos + '\\)', 'g')
-      vm.value = vm.value.replace(reg, '')
+    }
+    // save (input, mk) {
+    //   const vm = this
+    //   Axios.post('http://192.168.1.98:4000/v1/article/edit', {
+    //     id: '598542f7e3995d378ec0b829',
+    //     content: vm.value,
+    //     pics: vm.img_file
+    //   })
+    //   .then(function (res) {
+    //     console.log(res)
+    //   })
+    // },
+    // saveImg (name, $file) {
+    //   const vm = this
+    //   let tem = name
+    //   const img = document.querySelector('img[rel="' + name + '"]')
+    //   const image = new Image()
+    //   image.onload = () => {
+    //     const canvas = document.createElement('canvas')
+    //     const ctx = canvas.getContext('2d')
+    //     const w = canvas.width = image.width
+    //     const h = canvas.height = image.height
+    //     ctx.drawImage(image, 0, 0, w, h, 0, 0, w, h)
+    //     const data = canvas.toDataURL('image/jpeg', 0.5)
+    //     // 去重
+    //     Object.keys(vm.img_file).map((pos) => {
+    //       if (vm.img_file[pos] === data) {
+    //         const reg = new RegExp('(!\\[[\\w\\W]+?\\]\\()' + name + '(\\))', 'g')
+    //         vm.value = vm.value.replace(reg, '$1' + pos + '$2')
+    //         tem = pos
+    //         return
+    //       }
+    //     })
+    //     if (tem === name) {
+    //       console.log(1)
+    //       vm.img_file[name] = data
+    //     }
+    //   }
+    //   image.src = img.src
+    // },
+    // imgDel (pos) {
+    //   const vm = this
+    //   delete this.img_file[pos]
+    //   const reg = new RegExp('!\\[[\\w\\W]+?\\]\\(' + pos + '\\)', 'g')
+    //   vm.value = vm.value.replace(reg, '')
+    // }
+  },
+  watch: {
+    id (value) {
+      this.getContent()
     }
   }
 }
