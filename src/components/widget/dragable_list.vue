9f2702625f989b851a33df51169487509937b6cc
Vongola <1085280247@qq.com>
2017-08-23 09:27:21 +0800

    目录查询修改功能

--- src/components/widget/dragable_list.vue
+++ src/components/widget/dragable_list.vue
@@ -2,20 +2,20 @@
   <div id="dragCol">
     <h2 @click="addTitle"><i class="el-icon-plus"></i>&nbsp;&nbsp;新标题</h2>
     <draggable class="wrapper" element="div" v-model="collection" :options="dragOptions"> 
-        <li class="col" v-for="(col, index) of collection" :key="col"> 
-          <p class="level-one" :title="col">
-            <span>{{col}}</span>
+        <li class="col" v-for="(col, index) of collection" :key="col.pri_dir"> 
+          <p class="level-one" :title="col.pri_dir">
+            <span>{{col.pri_dir}}</span>
             <i class="el-icon-delete2" title="删除目录" @click="delTitle(index)"></i>        
-            <i class="el-icon-plus" title="添加文档"　@click="addArticle(collection[index])"></i>      
+            <!-- <i class="el-icon-plus" title="添加文档"　@click="addArticle(collection[index])"></i>       -->
           </p>
           <el-collapse-transition>
-            <draggable element="div" v-model="collection1[collection[index]]" :options="dragOptions1" v-show="!show[index]"> 
+            <draggable element="div" v-model="collection[index].sec_dir" :options="dragOptions1" v-show="!show[index]"> 
               <transition-group type="transition" :name="'flip-list'">
-                <li class="level-two" v-for="(co, index1) of collection1[collection[index]]" :key="co"  :ref="index + '' + index1" @click="makeSelected(index + '' + index1)">
-                  <span :title="co">
-                    {{co}}        
+                <li class="level-two" v-for="(co, index1) of collection[index].sec_dir" :key="co._id"  :ref="index + '' + index1" @click="makeSelected(index + '' + index1, co)">
+                  <span :title="co.title">
+                    {{co.title}}        
                   </span>
-                  <i class="el-icon-delete2" title="删除文章" @click="delArticle(collection[index], index1)"></i>        
+                  <i class="el-icon-delete2" title="删除文章" @click="delArticle(index, index1)"></i> 
                 </li> 
               </transition-group>
             </draggable>
@@ -29,20 +29,33 @@
 import {Icon, MessageBox, Message} from 'element-ui'
 import draggable from 'vuedraggable'
 import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
+import { getBook } from '../../js/axios.js'
+
 export default {
   name: 'list-collection',
   data () {
     return {
-      collection: ['fsdf', 'asdfadf', '你是大厦斯蒂芬'],
-      collection1: {},
+      collection: [],
       editable: true,
       show: []
     }
   },
   mounted () {
-    this.sec_dir()
+    const vm = this
+    vm.getDir('599bd236fa367e6cf141118f')
   },
   methods: {
+    // 获取目录
+    getDir (id) {
+      let vm = this
+      getBook(id)
+      .then((res) => {
+        if (res.data.status === 200) {
+          vm.collection = res.data.data.pub.directory
+          vm.$emit('getDir', vm.collection)
+        }
+      })
+    },
     delTitle (index) {
       let vm = this
       MessageBox.confirm('此操作将删除该目录, 是否继续?', '提示', {
@@ -51,24 +64,27 @@ export default {
         type: 'warning'
       }).then(() => {
         vm.collection.splice(index, 1)
+        vm.$emit('getDir', vm.collection)
         Message({
           type: 'success',
           message: '删除成功!'
         })
-      }).catch(() => {
+      }).catch((err) => {
+        console.log(err)
         Message({
           type: 'info',
           message: '已取消删除'
         })
       })
     },
-    makeSelected (key) {
+    makeSelected (key, article) {
       let vm = this
       Object.keys(vm.$refs).map(ele => {
         if (!vm.$refs[ele][0]) return
         vm.$refs[ele][0].classList.remove('selected')
       })
       vm.$refs[key][0].classList.add('selected')
+      vm.$emit('selected', article)
     },
     addTitle () {
       let vm = this
@@ -78,7 +94,8 @@ export default {
         inputPattern: /\D/,
         inputErrorMessage: '标题名不能有数字'
       }).then(({ value }) => {
-        vm.collection.splice(vm.collection.length, 1, value)
+        vm.collection.splice(vm.collection.length, 1, { pri_dir: value, sec_dir: [] })
+        vm.$emit('getDir', vm.collection)
         Message({
           type: 'success',
           message: '添加成功'
@@ -90,34 +107,35 @@ export default {
         })
       })
     },
-    addArticle (key) {
-      let vm = this
-      MessageBox.prompt('请输入文档名称', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        inputPattern: /[\S]/,
-        inputErrorMessage: '文档名不能为空'
-      }).then(({ value }) => {
-        vm.collection1[key].splice(vm.collection1[key].length, 1, value)
-        Message({
-          type: 'success',
-          message: '添加成功'
-        })
-      }).catch(() => {
-        Message({
-          type: 'info',
-          message: '取消输入'
-        })
-      })
-    },
+    // addArticle (key) {
+    //   let vm = this
+    //   MessageBox.prompt('请输入文档名称', '提示', {
+    //     confirmButtonText: '确定',
+    //     cancelButtonText: '取消',
+    //     inputPattern: /[\S]/,
+    //     inputErrorMessage: '文档名不能为空'
+    //   }).then(({ value }) => {
+    //     vm.collection1[key].splice(vm.collection1[key].length, 1, value)
+    //     Message({
+    //       type: 'success',
+    //       message: '添加成功'
+    //     })
+    //   }).catch(() => {
+    //     Message({
+    //       type: 'info',
+    //       message: '取消输入'
+    //     })
+    //   })
+    // },
     delArticle (key, index) {
       let vm = this
-      MessageBox.confirm('此操作将删除该目录, 是否继续?', '提示', {
+      MessageBox.confirm('此操作将从目录下删除该文档, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        vm.collection1[key].splice(index, 1)
+        vm.collection[key].sec_dir.splice(index, 1)
+        vm.$emit('getDir', vm.collection)
         Message({
           type: 'success',
           message: '删除成功!'
@@ -128,14 +146,6 @@ export default {
           message: '已取消删除'
         })
       })
-    },
-    sec_dir () {
-      let vm = this
-      let obj = {}
-      for (let index in vm.collection) {
-        obj[vm.collection[index]] = [index, index + 1, index + 2]
-      }
-      vm.collection1 = obj
     }
   },
   computed: {
@@ -188,9 +198,12 @@ export default {
   margin: 0;
   padding: 0 5px 0 15px; 
 }
-#dragCol .level-one + div{
+ #dragCol .level-one + div > span{
+  display: inline-block;
+  min-height: 25px;
+  width: 100%;
   overflow: hidden;
-}
+} 
 #dragCol .level-one>span, #dragCol .level-two>span{
   display: inline-block;
   text-indent: 0;
