402d557fd4bdb525956e1623041adb022b0ae4e8
Vongola <1085280247@qq.com>
2017-08-23 09:27:49 +0800

    添加发布功能

--- src/components/repo-list/API_document.vue
+++ src/components/repo-list/API_document.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="repoDocument">
-    <doc-header title="穿云箭API文档" :pub="true" :preview="true"></doc-header>
-    <drag-list></drag-list>
+    <doc-header :title="docTitle" :pub="true" :preview="true" @pub="pubBook"></doc-header>
+    <drag-list @selected="getArticle" @getDir="getDir"></drag-list>
     <search label="API" @selected="getArticle"></search>
     <div>
       <mk-header :showTag="false" :title="title"></mk-header>
-      <editor :id="id"></editor>    
+      <editor :id="id" ></editor>    
     </div>
   </div>
 </template>
@@ -18,12 +18,16 @@ import search from '../widget/document_search'
 import mkHeader from '../widget/markdown_header'
 
 import draggable from 'vuedraggable'
+import { pubBook } from '../../js/axios.js'
 
 export default {
   name: 'repo_document',
   data () {
     return {
-      id: ''
+      id: '',
+      title: '',
+      docTitle: '穿云箭API文档',
+      collection: []
     }
   },
   methods: {
@@ -31,6 +35,21 @@ export default {
       let vm = this
       vm.id = article._id
       vm.title = article.title
+    },
+    pubBook (title, isSave) {
+      const vm = this
+      const levelTwo = {}
+      const levelOne = vm.collection.map((dir, index) => {
+        levelTwo[dir.pri_dir] = []
+        dir.sec_dir.map((seDir) => {
+          levelTwo[dir.pri_dir].push(seDir._id)
+        })
+        return dir.pri_dir
+      })
+      pubBook({id: '599bd236fa367e6cf141118f', title, levelOne, levelTwo, isSave})
+    },
+    getDir (collection) {
+      this.collection.push(...collection)
     }
   },
   components: {
@@ -45,7 +64,5 @@ export default {
 </script>
 
 <style scoped>
- #repoDocument {
 
- }
 </style>
