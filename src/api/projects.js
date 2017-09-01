9c2182af23e457f58840d9ae8b87db277e964777
Vongola <1085280247@qq.com>
2017-09-01 15:01:46 +0800

    修改添加项目、编辑项目参数判断

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,19 +1,19 @@
 import Axios from './index'
 
 // 获取项目列表
-function getProjectList ({page, limit = 10, status}) {
+function getProjectList ({page, limit = 10, status, name}) {
   return Axios.post('/project/query', {
-    page, limit, status
+    page, limit, status, name
   })
 }
 // 添加新项目
 function addProject ({ name, details, tag, serverUrl, cover }) {
   const formData = new FormData()
-  formData.append('name', name)
-  formData.append('details', details)
-  formData.append('tag', JSON.stringify(tag))
-  formData.append('serverUrl', serverUrl)
-  formData.append('cover', cover)
+  if (name) formData.append('name', name)
+  if (details) formData.append('details', details)
+  if (tag) formData.append('tag', JSON.stringify(tag))
+  if (serverUrl) formData.append('serverUrl', serverUrl)
+  if (cover) formData.append('cover', cover)
   return Axios.post('/project/add', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
@@ -37,14 +37,15 @@ function getDetails (id) {
   })
 }
 // 编辑项目
-function editProject ({ id, name, details, tag, serverUrl, cover }) {
+function editProject ({ id, name, details, tag, serverUrl, cover, status }) {
   const formData = new FormData()
-  formData.append('id', id)
-  formData.append('name', name)
-  formData.append('details', details)
-  formData.append('tag', JSON.stringify(tag))
-  formData.append('serverUrl', serverUrl)
-  formData.append('cover', cover)
+  if (id) formData.append('id', id)
+  if (name) formData.append('name', name)
+  if (details) formData.append('details', details)
+  if (tag) formData.append('tag', JSON.stringify(tag))
+  if (serverUrl) formData.append('serverUrl', serverUrl)
+  if (cover) formData.append('cover', cover)
+  if (status) formData.append('status', status)
   return Axios.post('/project/edit', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
