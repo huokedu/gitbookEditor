fc9ebaa744e13cd71ebcbbe0fd641c8c123e0c47
Vongola <1085280247@qq.com>
2017-12-22 17:18:02 +0800

    修改静态服务器路径

--- config/path.js
+++ config/path.js
@@ -1,5 +1,5 @@
 module.exports = {
   hostPath: process.env.NODE_ENV === 'production' ? 'https://admin.apis.sh' : 'http://192.168.1.99:8000',
   serverPath: process.env.NODE_ENV === 'production' ? 'https://srv.admin.apis.sh' : 'http://192.168.1.99:8808',
-  staticPath: process.env.NODE_ENV === 'production' ? 'https://srv.admin.apis.sh' : 'http://192.168.1.99:9999'
+  staticPath: process.env.NODE_ENV === 'production' ? 'https://srv.admin.apis.sh' : 'http://192.168.1.99:9999/APIS'
 }
